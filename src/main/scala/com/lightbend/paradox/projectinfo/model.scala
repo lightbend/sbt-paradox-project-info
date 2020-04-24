beda55389781cab4e4a33aaf6e9bbfdd7c07926b
Enno Runne <458526+ennru@users.noreply.github.com>
2020-04-24 16:58:19 +0200

    Updated links & plugins

--- src/main/scala/com/lightbend/paradox/projectinfo/model.scala
+++ src/main/scala/com/lightbend/paradox/projectinfo/model.scala
@@ -36,11 +36,11 @@ case class SbtValues(artifact: String,
 trait ReadinessLevel { def name: String }
 object ReadinessLevel {
   private def glossary(anchor: String, label: String): String =
-    s"""<a href="https://developer.lightbend.com/docs/lightbend-platform/introduction/getting-help/support-terminology.html#$anchor" target="_blank" rel="noopener">$label</a>""".stripMargin
+    s"""<a href="https://developer.lightbend.com/docs/introduction/getting-help/support-terminology.html#$anchor" target="_blank" rel="noopener">$label</a>""".stripMargin
 
   case object Supported extends ReadinessLevel {
     val name =
-      s"""${glossary("supported", "Supported")}, <a href="https://www.lightbend.com/lightbend-platform-subscription" target="_blank" rel="noopener">Lightbend Platform Subscription</a> provides support"""
+      s"""${glossary("supported", "Supported")}, <a href="https://www.lightbend.com/lightbend-subscription" target="_blank" rel="noopener">Lightbend Subscription</a> provides support"""
   }
   case object Certified extends ReadinessLevel {
     val name =
